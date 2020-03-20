7b7edda4c996d71b8c35fffe2b0b2a2abef595a6
Asafrose <asafr1993@gmail.com>
2020-03-20 13:13:14 +0200

    Feature | Add Civilians logic

--- src/AutomatedElectionsProject/Civilians.cpp
+++ src/AutomatedElectionsProject/Civilians.cpp
@@ -1,1 +1,24 @@
 ﻿#include "Civilians.h"
+
+void Civilians::AddCivilian(Civilian& civilian)
+{
+	if (_civilians == nullptr)
+	{
+		_civilians = new Civilian*[1];
+		_physicalCount = 1;
+	}
+	else if (_count == _physicalCount)
+	{
+		Civilian** newArray = new Civilian*[2*_physicalCount];
+		for (int i = 0; i < _count; ++i)
+		{
+			newArray[i] = _civilians[i];
+		}
+		delete[] _civilians;
+		_civilians = newArray;
+		_physicalCount *= 2;
+	}
+
+	_civilians[_count] = &civilian;
+	_count++;
+}
--- src/AutomatedElectionsProject/Civilians.h
+++ src/AutomatedElectionsProject/Civilians.h
@@ -7,11 +7,14 @@
 class Civilians
 {
 private:
-	Civilians** _civilians = nullptr;
+	Civilian** _civilians = nullptr;
 	int _count = 0;
+	int _physicalCount = 0;
 	
 public:
-	void AddCivilian(Civilian civilian);
-	Civilian& GetCivilian(int index);
+	void AddCivilian(Civilian& civilian);
+	Civilian& GetCivilian(int index) const;
+	int GetCount() const;
+	void Free() const;
 };
 #endif // CIVILIANS_H
